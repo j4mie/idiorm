cc263ea65a8d7c0f87ad91008dd702395cc41ba4
Jamie Matthews <jamie.matthews@gmail.com>
2010-10-22 02:32:04 +0100

    Convert tests to use Idiorm's new logging system

--- test/test_queries.php
+++ test/test_queries.php
@@ -13,10 +13,12 @@
     require_once dirname(__FILE__) . "/../paris.php";
     require_once dirname(__FILE__) . "/test_classes.php";
 
+    // Enable logging
+    ORM::configure('logging', true);
+
     // Set up the dummy database connection
-    $db = new DummyPDO();
+    $db = new DummyPDO('sqlite::memory:');
     ORM::set_db($db);
-    Tester::set_db($db);
 
     class Simple extends Model {
     }
@@ -47,7 +49,7 @@
     }
 
     Model::factory('ModelWithCustomTableAndCustomIdColumn')->find_one(5);
-    $expected = 'SELECT * FROM `custom_table` WHERE `custom_id_column` = "5"';
+    $expected = "SELECT * FROM `custom_table` WHERE `custom_id_column` = '5' LIMIT 1";
     Tester::check_equal("Custom ID column", $expected);
 
     class ModelWithFilters extends Model {
@@ -62,11 +64,11 @@
     }
 
     Model::factory('ModelWithFilters')->filter('name_is_fred')->find_many();
-    $expected = 'SELECT * FROM `model_with_filters` WHERE `name` = "Fred"';
+    $expected = "SELECT * FROM `model_with_filters` WHERE `name` = 'Fred'";
     Tester::check_equal("Filter with no arguments", $expected);
 
     Model::factory('ModelWithFilters')->filter('name_is', 'Bob')->find_many();
-    $expected = 'SELECT * FROM `model_with_filters` WHERE `name` = "Bob"';
+    $expected = "SELECT * FROM `model_with_filters` WHERE `name` = 'Bob'";
     Tester::check_equal("Filter with arguments", $expected);
 
     class Widget extends Model {
@@ -76,19 +78,19 @@
     $widget->name = "Fred";
     $widget->age = 10;
     $widget->save();
-    $expected = 'INSERT INTO `widget` (`name`, `age`) VALUES ("Fred", "10")';
+    $expected = "INSERT INTO `widget` (`name`, `age`) VALUES ('Fred', '10')";
     Tester::check_equal("Insert data", $expected);
 
     $widget = Model::factory('Widget')->find_one(1);
     $widget->name = "Fred";
     $widget->age = 10;
     $widget->save();
-    $expected = 'UPDATE `widget` SET `name` = "Fred", `age` = "10" WHERE `id` = "1"';
+    $expected = "UPDATE `widget` SET `name` = 'Fred', `age` = '10' WHERE `id` = '1'";
     Tester::check_equal("Update data", $expected);
 
     $widget = Model::factory('Widget')->find_one(1);
     $widget->delete();
-    $expected = 'DELETE FROM `widget` WHERE `id` = "1"';
+    $expected = "DELETE FROM `widget` WHERE `id` = '1'";
     Tester::check_equal("Delete data", $expected);
 
     class CustomIdWidget extends Model {
