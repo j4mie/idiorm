9b3f13cc585622ce82522d85c712022339cfc571
m4 <herr.offizier@gmail.com>
2013-09-12 17:38:25 +0400

    improved typecasting for aggregate db functions

--- idiorm.php
+++ idiorm.php
@@ -711,7 +711,10 @@
 
             $return_value = 0;
             if($result !== false && isset($result->$alias)) {
-                if((int) $result->$alias == (float) $result->$alias) {
+                if (!is_numeric($result->$alias)) {
+                    $return_value = $result->$alias;
+                }
+                elseif((int) $result->$alias == (float) $result->$alias) {
                     $return_value = (int) $result->$alias;
                 } else {
                     $return_value = (float) $result->$alias;
