f312b3bf0b9226683872fee77f0b22cbffbd3a44
Mitch <mitchell.cain@gmail.com>
2014-06-19 19:39:49 +1000

    offsetExists() should return true for null values, resolves #181

--- idiorm.php
+++ idiorm.php
@@ -2081,7 +2081,7 @@
         // --------------------- //
 
         public function offsetExists($key) {
-            return isset($this->_data[$key]);
+            return array_key_exists($key, $this->_data);
         }
 
         public function offsetGet($key) {
