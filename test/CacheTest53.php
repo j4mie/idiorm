36964bc990366e221f3ffb64ba55df26d68f06d4
Peter Ivanov <peter@microweber.com>
2014-06-20 22:12:50 +0300

    tabs

--- test/CacheTest53.php
+++ test/CacheTest53.php
@@ -39,7 +39,7 @@ class CacheTest53 extends PHPUnit_Framework_TestCase {
         });
         ORM::configure('cache_query_result', function ($cache_key, $value, $connection_name, $table_name) use ($phpunit, &$my_cache) {
             $phpunit->assertEquals(true, is_string($cache_key));
-			$phpunit->assertEquals('widget', $table_name);
+            $phpunit->assertEquals('widget', $table_name);
             $my_cache[$cache_key] = $value;
         });
         ORM::configure('check_query_cache', function ($cache_key, $connection_name) use ($phpunit, &$my_cache) {
