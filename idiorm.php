2e5b2a36bab7b83fbdcad481ece4072b7fe95bd4
dk <krivosheev@game-insight.com>
2018-04-04 16:29:46 +0300

    added return type in phpdoc

--- idiorm.php
+++ idiorm.php
@@ -2084,6 +2084,7 @@
          * as the first parameter and leave out the second parameter.
          * Flags the properties as 'dirty' so they will be saved to the
          * database when save() is called.
+         * @return $this
          */
         public function set($key, $value = null) {
             return $this->_set_orm_property($key, $value);
