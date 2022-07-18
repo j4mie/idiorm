d24346cd48c699d7708d661faff44c61328bc077
Simon Holywell <simon@holywell.com.au>
2022-07-18 13:39:02 +1000

    alias framework_testcase

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -124,3 +124,7 @@ class MockMsSqlPDO extends MockPDO {
     }
     
 }
+
+if (!class_exists('PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+}
